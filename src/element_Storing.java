b7beca6b323e17da37112d6f539c394d84f92433
Nimesh <nimesh2402@gmail.com>
2016-01-08 02:04:42 +0530

    First

---
+++ src/element_Storing.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.rules.Timeout;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class element_Storing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//ClearTrip Registration
+	WebDriver driver=new FirefoxDriver();
+	driver.navigate().to("http://www.makemytrip.com/flights");
+	
+	driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+	//String class1=driver.findElement(By.xpath(".//*[@id='class_selector']")).getText();
+	WebElement we=driver.findElement(By.xpath(".//*[@id='class_selector']"));
+	List<WebElement> lists = we.findElements(By.tagName("option"));
+    for(WebElement element: lists)  
+    {
+        String var2 = element.getText();
+        System.out.println(var2);
+    }
+		
+		
+		
+		
+	}
+
+}
