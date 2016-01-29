d051af4c7abc032b9ceade6241fd8ee204556cdd
Nimesh <nimesh2402@gmail.com>
2016-01-30 04:14:43 +0530

    d

--- src/element_Storing.java
+++ src/element_Storing.java
@@ -3,6 +3,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.junit.rules.Timeout;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -11,25 +12,36 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class element_Storing {
 
+	static WebDriver driver;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		//ClearTrip Registration
-	WebDriver driver=new FirefoxDriver();
+	driver=new FirefoxDriver();
 	driver.navigate().to("http://www.makemytrip.com/flights");
 	
 	driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 	//String class1=driver.findElement(By.xpath(".//*[@id='class_selector']")).getText();
+	
+	
 	WebElement we=driver.findElement(By.xpath(".//*[@id='class_selector']"));
+	
+	
+	JavascriptExecutor js = (JavascriptExecutor) driver;
+	//Applying boarder to the element
+	  js.executeScript("arguments[0].setAttribute('style', arguments[1]);", we, "color: red; border: 5px solid red;"); 
+	// js.executeScript("", arg1)
+	  //js.executeScript("arguments[0].setAttribute('style', arguments[1]);", we, ""); 
+	
 	List<WebElement> lists = we.findElements(By.tagName("option"));
-    for(WebElement element: lists)  
+    
+	for(WebElement element: lists)  
     {
         String var2 = element.getText();
         System.out.println(var2);
     }
 		
-		
-		
+
+	
 		
 	}
 
